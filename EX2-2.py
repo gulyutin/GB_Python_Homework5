ffe9a71f0efd90418f0be11a0796cecaf5e40552
Gulyutin <gulyutin@gmail.com>
2022-10-23 15:57:54 +0700

    EX-2

---
+++ EX2-2.py
@@ -0,0 +1,69 @@
+# Создайте программу для игры с конфетами человек против человека.
+
+# Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая 
+# ход друг после друга. Первый ход определяется жеребьёвкой. 
+# За один ход можно забрать не более чем 28 конфет. 
+# Все конфеты оппонента достаются сделавшему последний ход. 
+# Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
+
+# a) Добавьте игру против бота
+
+from random import randint, choice
+
+greeting = ('На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга. Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет. Все конфеты оппонента достаются сделавшему последний ход. ')
+  
+
+def introduce_players():
+    player1 = input('Давайте познакомися. Как Вас зовут?\n')
+    player2 = 'Бот'
+    print(f'Очень приятно, меня зовут {player2}')
+    return [player1, player2]
+
+
+def get_rules(players):
+    n = 1024
+    m = 28
+    first = int(input(
+        f'{players[0]}, если хотите ходить первым, нажмите 1, если нет, любую другую клавишу\n'))
+    if first != 1:
+        first = 0
+    return [n, m, int(first)]
+
+
+def play_game(rules, players):
+    count = rules[2]
+    if rules[0] % 10 == 1 and 9 > rules[0] > 10:
+        letter = 'а'
+    elif 1 < rules[0] % 10 < 5 and 9 > rules[0] > 10:
+        letter = 'ы'
+    else:
+        letter = ''
+    while rules[0] > 0:
+        if not count % 2:
+            move = randint(1, rules[1])
+            print(f'Я забираю {move}')
+        else:
+            print(f'{players[0]}, Ваш ход')
+            move = int(input())
+            if move > rules[0] or move > rules[1]:
+                print(f'Это слишком много, можно взять не более {rules[1]} конфет{letter}, у нас всего {rules[0]} конфет{letter}')
+        rules[0] = rules[0] - move
+        if rules[0] > 0:
+            print(f'Осталось {rules[0]} конфет{letter}')
+        else:
+            print('Все конфеты разобраны.')
+        count += 1
+    return players[count % 2]
+
+
+print(greeting)
+
+players = introduce_players()
+rules = get_rules(players)
+
+winer = play_game(rules, players)
+if not winer:
+    print('У нас нет победителя.')
+else:
+    print(
+        f'Поздравляю! В этот раз победил {winer}! Ему достаются все конфеты!')
